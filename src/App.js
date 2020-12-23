ddccdc1fce62e326c40f100ee6368ad9cae059c4
keyysa <apt.muenchen@gmail.com>
2020-12-23 17:35:50 -0500

    2. About Section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+// Import Pages
+import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <AboutUs />
     </div>
   );
 }
