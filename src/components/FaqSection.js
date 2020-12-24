b3ebc1c8a9595c1851664fd132709eadd923d575
keyysa <apt.muenchen@gmail.com>
2020-12-23 21:27:13 -0500

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,56 @@
+import React from "react";
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+            possimus veniam eligendi temporibus quidem enim ut labore cum earum
+            corporis.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily schedule?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+            possimus veniam eligendi temporibus quidem enim ut labore cum earum
+            corporis.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Payment method?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+            possimus veniam eligendi temporibus quidem enim ut labore cum earum
+            corporis.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Which product do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+            possimus veniam eligendi temporibus quidem enim ut labore cum earum
+            corporis.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
