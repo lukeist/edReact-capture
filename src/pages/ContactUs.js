1ad10431f493e9c20503d400ed54bcef7c1e0435
keyysa <apt.muenchen@gmail.com>
2020-12-27 01:29:39 -0500

    21. Contact Animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,21 +1,76 @@
 import React from "react";
 //Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAni } from "../animation";
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyle
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
       // style={{ background: "#fff" }}
     >
-      <h1>Contact</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAni}>Contact</motion.h2>
+        </Hide>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAni}>
+            <Circle />
+            <h2>Send Us a Message</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAni}>
+            <Circle />
+            <h2>Send an Email</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAni}>
+            <Circle />
+            <h2>Social Media</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
   );
 };
+
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;
